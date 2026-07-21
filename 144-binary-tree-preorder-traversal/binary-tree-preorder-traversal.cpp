a5ed130bcbfaceb147cffda24bca2a8cda5963f0
Nayana Chithra B <cbnayana2006@gmail.com>
2026-07-21 10:37:19 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (43.15%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -10,16 +10,19 @@
  * };
  */
 class Solution {
-    void helper(TreeNode* node, vector<int> &ans){
-        if(node== NULL) return ;
-        ans.push_back(node->val);
-        helper(node-> left, ans);
-        helper(node-> right, ans);
-    }
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        helper(root,ans);
-        return ans;
+        vector<int> preorder;
+        if(root==NULL) return preorder;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right!=NULL) st.push(root->right);
+            if(root->left!=NULL) st.push(root->left);
+            
+        } return preorder;
     }
 };
