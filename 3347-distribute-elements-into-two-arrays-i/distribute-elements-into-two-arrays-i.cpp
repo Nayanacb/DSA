8b30fa86197c8d09bc8482b06284a20a073e07b0
Nayana Chithra B <cbnayana2006@gmail.com>
2026-08-20 13:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.63%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> a1;
+        vector<int> a2;
+        a1.push_back(nums[0]);
+        a2.push_back(nums[1]);
+        int i=2;
+
+        while(i<n){
+            if(a1[a1.size()-1]>a2[a2.size()-1]) a1.push_back(nums[i]);
+            else a2.push_back(nums[i]);
+            i++;
+        }
+        a1.insert(a1.end(),a2.begin(), a2.end());
+        return a1;
+    }
+};
