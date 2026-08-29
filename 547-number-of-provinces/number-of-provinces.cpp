5f1f002f39f8a9d97e65e9101ac3a02a4805d05d
Nayana Chithra B <cbnayana2006@gmail.com>
2026-08-29 13:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (37.22%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void traversal(vector<vector<int>>& adj, int node, vector<int>& vis){
+        vis[node]=1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                traversal(adj, it, vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<int> vis(n,0);
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                cnt++;
+                traversal(adj, i, vis);
+            }
+        } return cnt;
+    }
+};
